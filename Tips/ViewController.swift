87ec9a4ed4831ee284497db331194a2360615222
Sunny Rochiramani <sunny@dropbox.com>
2015-04-16 13:52:01 -0700

    Initial tip calculator - follow video walkthrough

---
+++ Tips/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Tips
+//
+//  Created by Sunny Rochiramani on 4/16/15.
+//  Copyright (c) 2015 Sunny Rochiramani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func onBillAmountChanged(sender: AnyObject) {
+        println("Bill Amount Changed to " + billField.text);
+        
+        var tipPercentages = [0.18, 0.20, 0.30];
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onTipChanged(sender: AnyObject) {
+        onBillAmountChanged(sender)
+    }
+    
+}
+
